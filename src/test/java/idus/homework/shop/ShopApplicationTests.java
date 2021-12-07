0041a96ed4ae99aa35d270a207d679e0e8c7cc71
hanbee <sonhanbi1002@gmail.com>
2021-12-07 23:54:40 +0900

    프로젝트 생성

---
+++ src/test/java/idus/homework/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package idus.homework.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
