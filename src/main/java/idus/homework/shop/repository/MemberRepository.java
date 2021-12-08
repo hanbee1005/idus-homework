1e160ac01e573b500cb86419f69ffeba258052c0
hanbee <sonhanbi1002@gmail.com>
2021-12-08 21:41:01 +0900

    MemberRepository 생성

---
+++ src/main/java/idus/homework/shop/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package idus.homework.shop.repository;
+
+import idus.homework.shop.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
