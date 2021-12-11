00470f62b2c8072ce95dcb6e15955b34d6b1cec8
hanbee <sonhanbi1002@gmail.com>
2021-12-11 23:35:47 +0900

    로그인시 샘플 비밀번호 변경

--- src/main/java/idus/homework/shop/dto/JwtRequest.java
+++ src/main/java/idus/homework/shop/dto/JwtRequest.java
@@ -10,6 +10,6 @@ public class JwtRequest {
     @Schema(description = "이메일", example = "test@gmail.com")
     private String email;
 
-    @Schema(description = "비밀번호", example = "abcd1234ef")
+    @Schema(description = "비밀번호", example = "Abcd1234!@")
     private String password;
 }
