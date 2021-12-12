8c5ddfdb81fce1840562d1cbc47199d175ae5c46
hanbee <sonhanbi1002@gmail.com>
2021-12-13 06:36:07 +0900

    주문 목록 조회 테스트 코드 추가 작성

--- src/test/java/idus/homework/shop/service/OrderServiceTest.java
+++ src/test/java/idus/homework/shop/service/OrderServiceTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Slf4j
 @ExtendWith(SpringExtension.class)
@@ -31,22 +32,12 @@ public class OrderServiceTest {
 
     @BeforeEach
     void init() {
-        MemberSignupRequest request = new MemberSignupRequest();
-        request.setName("테스터1");
-        request.setNickname("tester");
-        request.setPassword("1234");
-        request.setPhoneNum("01012345678");
-        request.setEmail("test1@gmail.com");
-        request.setGender("F");
-
-        Member member = Member.builder().request(request).build();
-        memberRepository.save(member);
-
+        makeMember();
         makeOrders();
     }
 
     @Test
-    @DisplayName("단일 회원 주문 목록 조회")
+    @DisplayName("단일 회원 주문 목록 조회 - 주문 목록 o")
     void findOrders() {
         // given
         String email = "test1@gmail.com";
@@ -57,6 +48,58 @@ public class OrderServiceTest {
         // then
         assertThat(response.getEmail()).isEqualTo(email);
         assertThat(response.getOrders().size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("단일 회원 주문 목록 조회 - 주문 목록 x")
+    void findOrdersEmpty() {
+        // given
+        String email = "codetest@gmail.com";
+
+        // when
+        SearchOrderByEmailResponse response = orderService.findOrdersByEmail(email);
+
+        // then
+        assertThat(response.getEmail()).isEqualTo(email);
+        assertThat(response.getOrders().size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("단일 회원 주문 목록 조회 - 회원이 없는 경우")
+    void notFoundMember() {
+        // given
+        String email = "testcode@gmail.com";
+
+        // when
+        // then
+        assertThrows(IllegalStateException.class, () -> orderService.findOrdersByEmail(email));
+    }
+
+    private void makeMember() {
+        MemberSignupRequest request1 = new MemberSignupRequest();
+        request1.setName("테스터code");
+        request1.setNickname("tester");
+        request1.setPassword("Abcd1234!@");
+        request1.setPhoneNum("01012345678");
+        request1.setEmail("test1@gmail.com");
+        request1.setGender("F");
+
+        saveMember(request1);
+
+        MemberSignupRequest request2 = new MemberSignupRequest();
+        request2.setName("code테스터");
+        request2.setNickname("tester");
+        request2.setPassword("Abcd1234!@");
+        request2.setPhoneNum("01012345678");
+        request2.setEmail("codetest@gmail.com");
+        request2.setGender("M");
+
+        saveMember(request2);
+    }
+
+    private void saveMember(MemberSignupRequest request) {
+        Member member = Member.builder().request(request).build();
+        memberRepository.save(member);
     }
 
     private void makeOrders() {
