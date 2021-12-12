7b072dd013584f3a432ebcb842192eab2bd77a83
hanbee <sonhanbi1002@gmail.com>
2021-12-13 06:28:29 +0900

    없는 회원 조회 테스트 추가

--- src/test/java/idus/homework/shop/service/MemberServiceTest.java
+++ src/test/java/idus/homework/shop/service/MemberServiceTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Slf4j
 @ExtendWith(SpringExtension.class)
@@ -32,7 +33,7 @@ class MemberServiceTest {
         MemberSignupRequest request1 = new MemberSignupRequest();
         request1.setName("테스터");
         request1.setNickname("tester");
-        request1.setPassword("1234");
+        request1.setPassword("Abcd1234!@");
         request1.setPhoneNum("01012345678");
         request1.setEmail("test@gmail.com");
         request1.setGender("F");
@@ -42,7 +43,7 @@ class MemberServiceTest {
         MemberSignupRequest request2 = new MemberSignupRequest();
         request2.setName("홍길동");
         request2.setNickname("hong");
-        request2.setPassword("1234");
+        request2.setPassword("Abcd1234!@");
         request2.setPhoneNum("01012345678");
         request2.setEmail("hong@gmail.com");
         request2.setGender("M");
@@ -52,7 +53,7 @@ class MemberServiceTest {
         MemberSignupRequest request3 = new MemberSignupRequest();
         request3.setName("김영희");
         request3.setNickname("yhkim");
-        request3.setPassword("1234");
+        request3.setPassword("Abcd1234!@");
         request3.setPhoneNum("01012345678");
         request3.setEmail("yhkim@gmail.com");
         request3.setGender("F");
@@ -76,6 +77,17 @@ class MemberServiceTest {
 
         // then
         assertThat(findMember.getEmail()).isEqualTo(email);
+    }
+
+    @Test
+    @DisplayName("단일 회원 상세 정보 조회 - 회원이 없는 경우")
+    void notFoundMember() {
+        // given
+        String email = "testcode@gmail.com";
+
+        // when
+        // then
+        assertThrows(IllegalStateException.class, () -> memberService.findMemberById(email));
     }
 
     @Test
